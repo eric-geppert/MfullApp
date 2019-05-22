960ef2359ea2fa6688541c050f4ef447efcd0651
Eric <ericgeppert@att.net>
2019-05-22 14:09:55 -0500

    build production file included

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import * as serviceWorker from './serviceWorker';
+// import * as serviceWorker from './serviceWorker';
 // import { withRouter as Router } from "react-router-dom";
 import { BrowserRouter as Router } from "react-router-dom";
 import { Provider } from "react-redux";
@@ -21,4 +21,4 @@ ReactDOM.render(
 // If you want app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // service worker link: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+// serviceWorker.unregister();
