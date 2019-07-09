29c4519ca79bc48e35d4f853e577a6bc4be56464
Eric <ericgeppert@att.net>
2019-07-09 10:15:14 -0500

    had to fix the workouts again, because of bad merge

--- src/components/MyPDFDocument.js
+++ src/components/MyPDFDocument.js
@@ -2,9 +2,41 @@ import React, { Component } from "react";
 import { Document, Page } from 'react-pdf';
 
 class MyPDFDocument extends Component {
+    rednerWorkout(param) {
+        switch(param){
+            case("../images/workouts/ConditioningWeightLoss5V1.pdf"):
+                return(
+                    require("../images/workouts/ConditioningWeightLoss5V1.pdf")
+                );
+            case("../images/workouts/ConditioningWeightLoss4V1.pdf"):
+                return(
+                    require("../images/workouts/ConditioningWeightLoss4V1.pdf")
+                );
+            case("../images/workouts/ConditioningWeightLoss3V1.pdf"):
+                return(
+                    require("../images/workouts/ConditioningWeightLoss3V1.pdf")
+                );
+            case("../images/workouts/AtHomeTotalBody5V1.pdf"):
+                return(
+                    require("../images/workouts/AtHomeTotalBody5V1.pdf")
+                );
+            case("../images/workouts/AtHomeTotalBody4V1.pdf"):
+                return(
+                    require("../images/workouts/AtHomeTotalBody4V1.pdf")
+                );
+            case("../images/workouts/AtHomeTotalBody3V1.pdf"):
+                return(
+                    require("../images/workouts/AtHomeTotalBody3V1.pdf")
+                );
+            default:
+                console.log("workout Not found");
+        }
+    }
+
     render(){
         return(
-            <Document file={require("../images/handout.pdf")}>
+            // file={require("../images/handout.pdf")}
+            <Document file={this.rednerWorkout(this.props.file)}>
                 <Page pageNumber={1}
                 onLoadError={console.error}
                 />
