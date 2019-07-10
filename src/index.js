4672ee5e7c933b1841ba5f36c19a07286624d7b3
Eric <ericgeppert@att.net>
2019-07-10 17:16:35 -0500

    got videos embeded in movement, can't get them to space by row

--- src/index.js
+++ src/index.js
@@ -20,8 +20,16 @@ const backGroundStyle={
 ReactDOM.render(
   <Provider store={store} style={backGroundStyle}> {/* for redux store updates (needs to be on root component) */}
     {/* <Router>       */}
+    {/* changed for deploying on gh-pages */}
     <HashRouter>
-      <App />
+      <App >
+        {/* <link
+        rel="stylesheet"
+        href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
+        integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T"
+        crossorigin="anonymous"
+        /> */}
+      </App>
     </HashRouter>
     {/* </Router> */}
   </Provider>,
